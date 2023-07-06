bc2092303ecef1a7476b80ae7c0292e704c273ba
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-06 20:07:36 +0100

    Exercise 005 completed and tests passed

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -39,25 +39,65 @@ export const count1sand0s = (str) => {
 export const reverseNumber = (n) => {
 	if (n === undefined) throw new Error('n is required');
 	// Your code here!
+
+	const reverseString = n.toString().split('').reverse();
+	const reversedNumber = parseFloat(reverseString.join(''));
+	const finalNumber = reversedNumber * Math.sign(n);
+
+	return finalNumber;
 };
 
 export const sumArrays = (arrs) => {
 	if (arrs === undefined) throw new Error('arrs is required');
 	// Your code here!
+	let total = 0;
+
+	arrs.forEach((subArray) => {
+		const sum = subArray.reduce((acc, num) => acc + num, 0);
+		total += sum;
+	});
+
+	return total;
 };
 
 export const arrShift = (arr) => {
 	if (arr === undefined) throw new Error('arr is required');
 	// Your code here!'
+
+	[arr[0], arr[arr.length - 1]] = [arr[arr.length - 1], arr[0]];
+
+	return arr;
 };
 
 export const findNeedle = (haystack, searchTerm) => {
 	if (haystack === undefined) throw new Error('haystack is required');
 	if (searchTerm === undefined) throw new Error('searchTerm is required');
 	// Your code here!
+
+	const values = Object.values(haystack);
+
+	return values.some(value => value.toString().toLowerCase().includes(searchTerm.toLowerCase()));
 };
 
 export const getWordFrequencies = (str) => {
 	if (str === undefined) throw new Error('str is required');
 	// Your code here!
+
+	const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+	const cleanString = str.replace(punctuation, '').toLowerCase();
+
+	const words = cleanString.split(' ');
+
+	const wordFrequencies = {};
+
+	for (const word of words) {
+		if (wordFrequencies.hasOwnProperty(word)) {
+			wordFrequencies[word]++;
+		} else {
+			wordFrequencies[word] = 1;
+		}
+	}
+
+	return wordFrequencies;
 };
