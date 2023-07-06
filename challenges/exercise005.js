aa2659c53c82bf7d3f31de71bd85e40a19800aa3
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-06 01:35:37 +0100

    did some of exercise005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -2,11 +2,38 @@ export const findNextNumber = (nums, n) => {
 	if (nums === undefined) throw new Error('nums is required');
 	if (n === undefined) throw new Error('n is required');
 	// Your code here!
+
+	// Find the index of the target number in the array
+	const index = nums.findIndex(num => num == n);
+
+	// If the target number is not found or is at the last index, return null
+	if (index == -1 || index == nums.length - 1) {
+		return null;
+	}
+
+	// Return the number after the target number
+	return nums[index + 1];
 };
 
 export const count1sand0s = (str) => {
 	if (str === undefined) throw new Error('str is required');
 	// Your code here!
+	let countForZero = 0;
+	let countForOne = 0;
+
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === '0') {
+			countForZero++;
+		}
+		else if (str[i] === '1') {
+			countForOne++;
+		}
+	}
+
+	return {
+		1: countForOne,
+		0: countForZero
+	};
 };
 
 export const reverseNumber = (n) => {
@@ -21,7 +48,7 @@ export const sumArrays = (arrs) => {
 
 export const arrShift = (arr) => {
 	if (arr === undefined) throw new Error('arr is required');
-	// Your code here!
+	// Your code here!'
 };
 
 export const findNeedle = (haystack, searchTerm) => {
